449351d9fc67385a83c4f6f720cbc28e90fe8a06
mxJosS <jangel2003.kimba4@gmail.com>
2026-04-20 00:35:24 -0600

    feat: fetch product list from API

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,56 +1,76 @@
+import { useState, useEffect } from 'react';
 import './ProductList.css';
 
-
 const ProductList = () => {
+    const [productos, setProductos] = useState([]);
+    const [error, setError] = useState(null); 
+
+    useEffect(() => {
+        const fetchProductos = async () => {
+            try {
+                const response = await fetch("https://api-ten-jet.vercel.app/products");
+                
+                if (!response.ok) {
+                    throw new Error("Error al obtener los productos");
+                }
+                
+                const data = await response.json();
+           
+                setProductos(data); 
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        fetchProductos();
+    }, []);
+
     return (
         <section className='main-content'>
             <aside className='filters'>
-                <h2>
-                    Filtros
-                </h2>
+                <h2>Filtros</h2>
+                <div className="filters-category">
+                    <div className="filter-category">
+                        <h3>Categorías</h3>
+
+                        <label>
+                            <input type="checkbox" />
+                            <span>Hombres</span>
+                        </label>
+
+                        <label>
+                            <input type="checkbox" />
+                            <span>Niños</span>
+                        </label>
+
+                        <label>
+                            <input type="checkbox" />
+                            <span>Mujeres</span>
+                        </label>
+                    </div>
+
                     <div className="filters-category">
                         <div className="filter-category">
-                            <h3>Categorías</h3>
+                            <h3>Tipos</h3>
 
                             <label>
                                 <input type="checkbox" />
-                                <span>Hombres</span>
+                                <span>Prendas de abrigo</span>
                             </label>
 
                             <label>
                                 <input type="checkbox" />
-                                <span>Niños</span>
+                                <span>Ropa interior</span>
                             </label>
 
                             <label>
                                 <input type="checkbox" />
-                                <span>Mujeres</span>
+                                <span>Calzados</span>
                             </label>
                         </div>
-
-                            <div className="filters-category">
-                                <div className="filter-category">
-                                    <h3>Tipos</h3>
-
-                                    <label>
-                                        <input type="checkbox" />
-                                        <span>Prendas de abrigo</span>
-                                    </label>
-
-                                    <label>
-                                        <input type="checkbox" />
-                                        <span>Ropa interior</span>
-                                    </label>
-
-                                    <label>
-                                        <input type="checkbox" />
-                                        <span>Calzados</span>
-                                    </label>
-
-                                </div>
-                            </div>
                     </div>
+                </div>
             </aside>
+            
             <main className='collections'>
                 <div className="options">
                     <h2>TODAS LAS COLECCIONES</h2>
@@ -61,19 +81,31 @@ const ProductList = () => {
                                 <option>Relevantes</option>
                                 <option>Precio: Mayor a Menor</option>
                                 <option>Precio: Menor a Mayor</option>
-
                             </select>
                         </label>
                     </div>
                 </div>
+
+                <div className="products">
+                    {error ? (
+                        <p className='error-message'>{error}</p>
+                    ) : (
+                        productos.map((producto) => (
+                            <div className="product-card" key={producto.id}>
+                                <img 
+                                    src={producto.image} 
+                                    alt="Imagen del producto" 
+                                    className="product-image" 
+                                /> 
+                                <h3>{producto.nombre}</h3>
+                                <p>{producto.precio}</p>
+                            </div>
+                        ))
+                    )}
+                </div>
             </main>
-
         </section>
-
-        
-    )
-}
-
-
+    );
+};
 
 export default ProductList;
